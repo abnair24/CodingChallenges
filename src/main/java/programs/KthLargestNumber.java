10dd9e88471b0e9fe56415ac173a540ef1f87e44
Aswathy <aswathy.sreenath9@gmail.com>
2018-07-25 14:50:49 +0530

    KthLargest

---
+++ src/main/java/programs/KthLargestNumber.java
@@ -0,0 +1,59 @@
+package programs;
+
+public class KthLargestNumber {
+
+
+    public static void main(String[] args) {
+        int[] array = new int[]{7, 6, 5, 4, 3, 2, 1};
+        int[] temp = new int[array.length];
+
+
+        KthLargestNumber k = new KthLargestNumber();
+        System.out.println(k.largest(array,2,temp));
+    }
+
+    public int largest(int[] array,int k,int[] temp) {
+        sort(array,temp);
+        return array[array.length-k];
+    }
+
+    public void sort(int[] array,int[] temp) {
+        mergeSort(array,0,array.length-1,temp);
+    }
+
+    public void mergeSort(int[] array,int left, int right,int[] temp) {
+        if(left< right) {
+            int middle = (left + right)/2;
+            mergeSort(array,left,middle,temp);
+            mergeSort(array,middle+1,right,temp);
+            merge(array,left,middle,right,temp);
+        }
+    }
+
+    public void merge(int[] array,int left,int middle,int right,int[] temp){
+        for(int i= left;i<=right;i++){
+            temp[i]=array[i];
+        }
+
+        int le =left;
+        int mid = middle+1;
+        int k =left;
+
+        while(le <= middle && mid <= right) {
+            if(temp[le]<=temp[mid]){
+                array[k]=temp[le];
+                le++;
+            } else {
+                array[k]= temp[mid];
+                mid++;
+            }
+            k++;
+        }
+
+        while(le<=middle) {
+            array[k] = temp[le];
+            le++;
+            k++;
+        }
+    }
+}
