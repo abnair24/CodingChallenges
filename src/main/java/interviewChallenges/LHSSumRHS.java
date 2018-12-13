4bfc934b857cb9f18cc718d1dfc89f828c3db38d
Aswathy <aswathy.sreenath9@gmail.com>
2018-12-13 22:02:31 +0530

    Element dividing array

---
+++ src/main/java/interviewChallenges/LHSSumRHS.java
@@ -0,0 +1,38 @@
+package interviewChallenges;
+
+
+/*
+Given, an array of size n. Find an element which divides the array in two sub-arrays with equal sum.
+
+
+ */
+public class LHSSumRHS {
+
+    public static void main(String[] args) {
+
+        int[] array = {-1,3,0,1,4,-3};
+
+        LHSSumRHS lhsSumRHS = new LHSSumRHS();
+        System.out.println(lhsSumRHS.findElement(array));
+    }
+
+    public int findElement(int[] array) {
+
+        int rsum = 0;
+        int lsum = 0;
+
+        for(int i =1;i<array.length;i++) {
+            rsum += array[i];
+        }
+
+        for(int i =0,j=1; j< array.length; i++,j++) {
+            rsum = rsum - array[j];
+            lsum = lsum + array[i];
+
+            if(lsum == rsum) {
+                return array[j];
+            }
+        }
+        return -1;
+    }
+}
